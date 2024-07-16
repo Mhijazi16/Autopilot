b81971e0858f27176606ad2092d5310bb0cb9599
Mhijazi16 <gultimate0@gmail.com>
2024-07-16 14:29:10 +0300

    ShellAgent : adding describtion

--- ShellAgent/IO/ShellTools.py
+++ ShellAgent/IO/ShellTools.py
@@ -4,3 +4,8 @@ class ShellTools:
 
     @tool("Execute Linux Command")
     def execute_linux_command(self, command:str): 
+        """ This tool is used to execute simple linux commands on
+            the shell to use this tool you should pass the command
+            you want to execute as a string inside the arguments 
+            you should pass only the command nothing more nothing less
+            """
