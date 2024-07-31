696e058e2897d3492912b94a2611b7ef65c694ae
Mhijazi16 <gultimate0@gmail.com>
2024-07-31 21:08:36 +0300

    adding hash_to() tool

--- CryptoAgent/CryptoAgent.py
+++ CryptoAgent/CryptoAgent.py
@@ -2,6 +2,22 @@ from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.tools import tool
 from langchain_core.output_parsers import JsonOutputToolsParser
+import os
 
+@tool("hash_to")
+def hash_to(input: str, algorithm:str): 
+    """this tool takes two things 
+        input which is string of what you want to hash and 
+        algorithm which is string the type of hash 
+        you can choose from these two types of hashes
 
+        1) sha1sum 
+        2) md5sum 
+        3) sha224sum
+        4) sha256sum
+        5) sha384sum
+        6) sha512sum
 
+        the tool returns the result of hash"""
+
+    return os.popen(f"echo -n \'{input}\' | {algorithm}").read()
