2d78902c947136caf9274216b146c05ae6ca3fcd
Mhijazi16 <gultimate0@gmail.com>
2024-07-31 20:14:14 +0300

    Created CryptoAgent and imported libraries

---
+++ CryptoAgent/CryptoAgent.py
@@ -0,0 +1,7 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.tools import tool
+from langchain_core.output_parsers import JsonOutputToolsParser
+
+
+
