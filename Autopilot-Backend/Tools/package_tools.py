afd46415454f2764fe30763186a38c4b422dbb91
Mhijazi16 <gultimate0@gmail.com>
2024-10-11 13:59:58 +0300

    adding install_package() tools

--- Autopilot-Backend/Tools/package_tools.py
+++ Autopilot-Backend/Tools/package_tools.py
@@ -6,3 +6,23 @@ def handle_sudo(process: spawn):
     process.expect('password')
     process.sendline(str(password))
     return process
+
+def install_package(name: str): 
+    """
+        install_package this tool run the command 
+        'sudo pacman -S name' on the shell to install 
+        a software package on the system it takes in 
+        the name then returns the output of installation
+        Args: 
+            name: string
+        Returns: 
+            returns what happened after execution
+    """
+    process = spawn(f"sudo pacman -S {name}")
+    process = handle_sudo(process)
+    expectation = process.expect(["[Y/n]","target not found"])
+    if expectation == 0:
+        process.sendline("y")
+        process.expect("")
+    return process.read().decode()
+
