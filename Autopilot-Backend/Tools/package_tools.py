894750ac083cfd51583fbb3b3b252bd5b0319c59
Mhijazi16 <gultimate0@gmail.com>
2024-10-11 17:12:43 +0300

    installing list of packages

--- Autopilot-Backend/Tools/package_tools.py
+++ Autopilot-Backend/Tools/package_tools.py
@@ -7,17 +7,21 @@ def handle_sudo(process: spawn):
     process.sendline(str(password))
     return process
 
-def install_package(name: str): 
+def install_packages(names: list[str]): 
     """
         install_package this tool run the command 
-        'sudo pacman -S name' on the shell to install 
-        a software package on the system it takes in 
-        the name then returns the output of installation
+        'sudo pacman -S name1 name2 ...nameN' on the shell 
+        to install the given software package on the system 
+        it takes in the names of packages you want to install 
+        then returns the output of installation
         Args: 
-            name: string
+            names : list of strings
         Returns: 
             returns what happened after execution
     """
-    process = spawn(f"sudo pacman --noconfirm -S {name}")
+    command = f"sudo pacman --noconfirm -S {" ".join(names)}"
+    process = spawn(command)
     process = handle_sudo(process)
     return process.read().decode()
+
+print(install_packages(["neofetch","fastfetch"]))
