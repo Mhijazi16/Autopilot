6cdac4d4d0f7ad05c81ac428d924ba29c91bc3d4
Mhijazi16 <gultimate0@gmail.com>
2024-10-18 16:46:52 +0300

    add tool description

--- Autopilot-Backend/Tools/network_tools.py
+++ Autopilot-Backend/Tools/network_tools.py
@@ -1,11 +1,23 @@
 from pexpect import pxssh
-
-PROMPT = []
+import os
 
 def read_status(process):
     return process.before.decode()
 
 def ssh(username: str, hostname: str, password, commands: list[str]):
+    """
+        ssh is a tool that connectes and executes 
+        commands to remote host or server.
+        it takes in a list of commands
+        Args: 
+            username: str 
+            hostname: str
+            password: str 
+            commands: list of str
+        Returns: 
+            the output of executing commands on remote 
+            server
+    """
     session = pxssh.pxssh()
     try:
         session.login(hostname,username,password)
