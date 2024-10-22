c98daebbbd7d29a92813a351b8f6ec7deb7901a6
Mhijazi16 <gultimate0@gmail.com>
2024-10-22 11:45:39 +0300

    add remove_users() tool

--- Autopilot-Backend/Tools/users_tools.py
+++ Autopilot-Backend/Tools/users_tools.py
@@ -37,3 +37,38 @@ def create_users(usernames: list[str], passwords: list[str]):
             output += f"🚨 faild creating {username} as a user.\n"
 
     return f"The result of the process:\n{output}"
+
+def remove_users(usernames: list[str]): 
+    """
+        This tool is used to remove one or more
+        user from the linux system 
+        Args: 
+            usernames: list of strings 
+        Example: 
+            usernaems = ["user1", "user2"]
+    """
+
+    if len(usernames) == 0: 
+        return "⚠️ Error: no usernames were supplied!" 
+    
+    successful_removes = []
+    command = "sudo userdel -r "
+    output = ""
+
+    for username in usernames: 
+        try: 
+            cmd = command + username 
+            child = pexpect.spawn(cmd)
+            child, message = handle_sudo(child)
+
+            if "granted" in message: 
+                issue = f"userdel: user '{username}' does not exist"
+                if  issue in read_status(child): 
+                    output += f"🚨 user by the name of {username} doesn't exist.\n"
+                else: 
+                    successful_removes.append(username)
+                    output += f"✅ user by the name of {username} was removed.\n"
+        except: 
+            output += f"🚨 faild removing {username}.\n"
+
+    return f"The result of the process:\n{output}"
