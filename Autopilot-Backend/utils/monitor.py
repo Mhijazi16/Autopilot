58a5d98af2911d56ab3841609c460ec0cb729395
Mhijazi16 <gultimate0@gmail.com>
2024-11-16 10:39:14 +0200

    add get_gpu_usage()

--- Autopilot-Backend/utils/monitor.py
+++ Autopilot-Backend/utils/monitor.py
@@ -9,3 +9,9 @@ def get_ram_usage():
     ram = psutil.virtual_memory()
     return ram.percent
 
+def get_gpu_usage():
+    gpus = GPUtil.getGPUs()
+    for gpu in gpus:
+        if gpu.name == "NVIDIA GeForce RTX 3060 Laptop GPU": 
+            return gpu.load * 100
+    return 0
