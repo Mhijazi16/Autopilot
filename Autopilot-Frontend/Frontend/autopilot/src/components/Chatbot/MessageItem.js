997da4903eed01e870828a06f6e9f15431fad203
momen <momenmarwanh@gmail.com>
2024-11-16 12:02:16 +0200

    Add syntax highliting

--- Autopilot-Frontend/Frontend/autopilot/src/components/Chatbot/MessageItem.js
+++ Autopilot-Frontend/Frontend/autopilot/src/components/Chatbot/MessageItem.js
@@ -3,10 +3,21 @@ import botLogo from '../../assets/icons/bot-logo.png';
 import './MessageItem.css';
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypePrism from 'rehype-prism';
 
 
 const MessageItem = ({ msg }) => {
 
+  const markdownText = ({text}) => {
+    return (
+      <Markdown 
+        children={text}
+        remarkPlugins={remarkGfm}
+        // rehypePlugins={rehypePrism}
+      >
+      </Markdown>
+    );
+  }
   return (
     <div className={`message-item ${msg.sender}`}>
       {msg.sender === 'bot' && (
@@ -14,9 +25,8 @@ const MessageItem = ({ msg }) => {
           <img src={botLogo} alt="Bot Icon" />
         </div>
       )}
-      <span className="message-text">{msg.sender === 'bot' ? (
-          <Markdown remarkPlugins={[remarkGfm]}>{msg.text}</Markdown>
-        ) : (
+      <span className="message-text">
+        {msg.sender === 'bot' ? markdownText(msg.text) : (
           <span>{msg.text}</span>
         )}</span>
       {msg.sender === 'user' && <div className="spacer" />}
