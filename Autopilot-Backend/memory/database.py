359024c670fcbc68603ffbebc5bf46e51632d7e1
Mhijazi16 <gultimate0@gmail.com>
2024-11-19 16:02:49 +0200

    finalizaing

--- Autopilot-Backend/memory/database.py
+++ Autopilot-Backend/memory/database.py
@@ -1,37 +1,41 @@
+from pydantic import BaseModel
 import redis
 import time
 import os
 
-
 memory = None
+
+class ToolbarSchema(BaseModel):
+    Navigation: str = "Off"
+    Coder: str = "Off"
+    Shell: str = "Off"
+    Github: str = "Off"
+    Users: str = "Off"
+    Monitor: str = "Off"
+    Packages: str = "Off"
+    Network: str = "Off"
+    Troubleshooter: str = "Off"
+
 def init():
     global memory
     os.system("redis-server &")
     time.sleep(2)
-    memory = redis.Redis(host='localhost',
-                         port=6379,
-                         decode_responses=True)
+    memory = redis.Redis(
+        host='localhost',
+        port=6379,
+        decode_responses=True
+    )
 
     populate_memory()
     return memory
 
 def populate_memory():
     global memory
-    toolbar = {
-        "Navigation": "False", 
-        "Coder": "False",
-        "Shell": "False",
-        "Github": "False",
-        "Users": "False",
-        "Monitor": "False",
-        "Packages": "False",
-        "Network": "False",
-        "Troubleshooter": "False",
-    }
+    toolbar = ToolbarSchema()
 
     if memory:
-        memory.set("feedback", "False")
-        memory.hset("toolbar", mapping=toolbar)
+        memory.set("feedback", "Off")
+        memory.hset("toolbar", mapping=toolbar.model_dump())
 
     return memory
 
