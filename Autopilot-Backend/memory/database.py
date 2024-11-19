ccfe4dbb9f57661171bd8a730a075f47877663ab
Mhijazi16 <gultimate0@gmail.com>
2024-11-19 14:27:22 +0200

    populating memory

--- Autopilot-Backend/memory/database.py
+++ Autopilot-Backend/memory/database.py
@@ -8,4 +8,22 @@ def init():
     memory = redis.Redis(host='localhost',
                          port=6379,
                          decode_responses=True)
+
+    return populate_memory(memory)
+
+def populate_memory(memory):
+    toolbar = {
+        "Navigation": False, 
+        "Coder": False,
+        "Shell": False,
+        "Github": False,
+        "Users": False,
+        "Monitor": False,
+        "Packages": False,
+        "Network": False,
+        "Troubleshooter": False,
+    }
+
+    memory.set("feedback", True)
+    memory.hset("toolbar", mapping=toolbar)
     return memory
