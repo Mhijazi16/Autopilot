1252e802bea04450e48a8a4666225434b1f2711e
Mhijazi16 <gultimate0@gmail.com>
2024-11-19 14:51:12 +0200

    adding stuff

--- Autopilot-Backend/memory/database.py
+++ Autopilot-Backend/memory/database.py
@@ -2,16 +2,21 @@ import redis
 import time
 import os
 
+
+memory = None
 def init():
+    global memory
     os.system("redis-server &")
     time.sleep(2)
     memory = redis.Redis(host='localhost',
                          port=6379,
                          decode_responses=True)
 
-    return populate_memory(memory)
+    populate_memory()
+    return memory
 
-def populate_memory(memory):
+def populate_memory():
+    global memory
     toolbar = {
         "Navigation": "False", 
         "Coder": "False",
@@ -24,6 +29,11 @@ def populate_memory(memory):
         "Troubleshooter": "False",
     }
 
-    memory.set("feedback", "False")
-    memory.hset("toolbar", mapping=toolbar)
+    if memory:
+        memory.set("feedback", "False")
+        memory.hset("toolbar", mapping=toolbar)
+
+    return memory
+
+def get_memory():
     return memory
