2b39c925b63ee421ebc38b005eb9529412f9cf22
Mhijazi16 <gultimate0@gmail.com>
2024-11-19 16:03:35 +0200

    adding endpoints

--- Autopilot-Backend/autopilot.py
+++ Autopilot-Backend/autopilot.py
@@ -1,11 +1,31 @@
 from utils.monitor import get_specs
-from memory.database import init
-from fastapi import FastAPI, WebSocket
+from memory.database import init, ToolbarSchema
+from fastapi import FastAPI, HTTPException, WebSocket
 import asyncio
 import json
 
+
 memory = init()
 app = FastAPI() 
+
+@app.post("/toolbar")
+async def set_toolbar(toolbar: ToolbarSchema): 
+    try: 
+        status = toolbar.model_dump()
+        memory.hset("toolbar", mapping=status)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error saving feedback: {e}")
+
+    return memory.hgetall("toolbar")
+
+@app.post("/feedback")
+async def set_feedback(feedback: str): 
+    try:
+        memory.set("feedback", feedback)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error saving feedback: {e}")
+
+    return {"feedback": memory.get("feedback")}
 
 @app.websocket("/monitor")
 async def monitor_socket(websocket: WebSocket):
