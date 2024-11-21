cb4f33667e172168bca56bf4a0c1503337f48ac5
Mhijazi16 <gultimate0@gmail.com>
2024-11-21 21:18:36 +0200

    add Plan class for structured outputs

--- Autopilot-Backend/states/planner.py
+++ Autopilot-Backend/states/planner.py
@@ -5,3 +5,8 @@ class Step(BaseModel):
     """ Step is the name of Tool and parameters that should be sent to that tool"""
     tool: str = Field("the name of tool should be executed in this step")
     params: str = Field("parameters that should be sent to this tool")
+
+class Plan(BaseModel): 
+    """ plan is order steps that should execute to meet objective"""
+    steps: List[Step] = Field("List of Tools that should be executed in order")
+
