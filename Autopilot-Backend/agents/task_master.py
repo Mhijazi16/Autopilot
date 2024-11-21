56ccc3ba7f13bde02157887185281af7f475142c
Mhijazi16 <gultimate0@gmail.com>
2024-11-21 22:39:43 +0200

    adding needed fields

--- Autopilot-Backend/agents/task_master.py
+++ Autopilot-Backend/agents/task_master.py
@@ -3,3 +3,6 @@ from langchain_core.prompts.chat import HumanMessagePromptTemplate
 from langchain_groq.chat_models import ChatGroq
 from langchain.prompts import ChatPromptTemplate
 from states.planner import Plan
+
+class TaskMaster():
+    __slots__ = ("llm", "template")
