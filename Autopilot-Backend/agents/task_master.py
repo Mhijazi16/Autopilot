8c70d704bcafb85d5720f2821cb940dc2bb4a71f
Mhijazi16 <gultimate0@gmail.com>
2024-11-21 22:39:13 +0200

    importing what is needed

---
+++ Autopilot-Backend/agents/task_master.py
@@ -0,0 +1,5 @@
+from langchain_core.messages import SystemMessage
+from langchain_core.prompts.chat import HumanMessagePromptTemplate
+from langchain_groq.chat_models import ChatGroq
+from langchain.prompts import ChatPromptTemplate
+from states.planner import Plan
