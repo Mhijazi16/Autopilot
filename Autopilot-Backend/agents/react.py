71d6d1f1922b3eadd99e9c9aa02d2978a3f7e3b6
Mhijazi16 <gultimate0@gmail.com>
2024-12-12 14:41:10 +0200

    create ReactAgent() class

--- Autopilot-Backend/agents/react.py
+++ Autopilot-Backend/agents/react.py
@@ -11,3 +11,19 @@ import asyncio
 class AgentState(MessagesState): 
     is_last_step: IsLastStep
     tool: tuple
+
+class ReactAgent():
+    def __init__(self, model, tools, config):
+        self.memory = get_memory()
+        self.tools = tools
+        self.checkpointer = MemorySaver()
+        self.config = config
+
+        self.llm = ChatOllama(model=model,
+                              temperature=0).bind_tools(tools)
+
+        self.agent = create_react_agent(model=self.llm,
+                              tools=tools,
+                              interrupt_before=['tools'],
+                              checkpointer=self.checkpointer,
+                              state_schema=AgentState)
