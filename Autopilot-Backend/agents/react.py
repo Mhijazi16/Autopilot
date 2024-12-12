966ae02e760c0c8080d510239ad8d1f08d6bef4b
Mhijazi16 <gultimate0@gmail.com>
2024-12-12 14:40:46 +0200

    defining a state

--- Autopilot-Backend/agents/react.py
+++ Autopilot-Backend/agents/react.py
@@ -7,3 +7,7 @@ from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 from toolkits.package_toolkit import get_package_toolkit
 import asyncio
+
+class AgentState(MessagesState): 
+    is_last_step: IsLastStep
+    tool: tuple
