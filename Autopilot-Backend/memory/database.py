ae1badd1ed7b5382b56fed6e1c5fd78782174f58
Mhijazi16 <gultimate0@gmail.com>
2024-12-12 12:21:15 +0200

    refactoring database

--- Autopilot-Backend/memory/database.py
+++ Autopilot-Backend/memory/database.py
@@ -1,10 +1,10 @@
+from redis.client import Redis
 from typing import Literal
 from pydantic import BaseModel
 import redis
-import time
 import os
+import time
 
-memory = None
 
 class ToolbarSchema(BaseModel):
     Navigation: Literal["On","Off"] = "Off"
@@ -17,28 +17,29 @@ class ToolbarSchema(BaseModel):
     Network: Literal["On","Off"] = "Off"
     Troubleshooter: Literal["On","Off"] = "Off"
 
-def init():
-    global memory
+def init() -> Redis:
     os.system("redis-server &")
     time.sleep(2)
+
     memory = redis.Redis(
         host='localhost',
         port=6379,
         decode_responses=True
     )
 
-    populate_memory()
+    populate_memory(memory)
     return memory
 
-def populate_memory():
-    global memory
+def populate_memory(memory: Redis) -> None:
     toolbar = ToolbarSchema()
+    memory.set("feedback", "Off")
+    memory.set("command", "not-set")
+    memory.set("status", "not-set")
+    memory.hset("toolbar", mapping=toolbar.model_dump())
 
-    if memory:
-        memory.set("feedback", "Off")
-        memory.hset("toolbar", mapping=toolbar.model_dump())
-
-    return memory
-
-def get_memory():
-    return memory
+def get_memory() -> Redis:
+    return redis.Redis(
+        host='localhost',
+        port=6379,
+        decode_responses=True
+    )
