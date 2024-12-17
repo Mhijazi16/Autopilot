6ea5455fc7e87bd98703a5020296518bbbdc0954
Mhijazi16 <gultimate0@gmail.com>
2024-12-17 15:38:01 +0200

    added filesystem toolkit

---
+++ Autopilot-Backend/toolkits/filesystem_toolkit.py
@@ -0,0 +1,28 @@
+import subprocess
+
+def run_command(command):
+    output = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.DEVNULL)
+    return output.strip()
+
+def open_folder(path: str): 
+    """
+    this tool is used to open a folder 
+    in the linux file system 
+    Args: 
+        path: str
+    Example: 
+        if user wants to open Videos folder
+        you should provide the following path 
+        /home/ha1st/Videos
+        and if we need to open Music
+        then: 
+        /home/ha1st/Music
+        else just provide the specific path 
+        of the folder you want to open
+    """
+
+    run_command(f"nautilus {path} > /dev/null 2>&1 &")
+    output = "The folder was opened and this is what the user sees:\n"
+    output += run_command(f"ls {path}")
+    return output
+
