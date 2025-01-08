23600d86b614d21bc7fa7ef1c274576975a80ca4
Mhijazi16 <gultimate0@gmail.com>
2025-01-08 10:56:20 +0200

    importing needed stuff

--- Autopilot-Backend/agents/shell_agent.py
+++ Autopilot-Backend/agents/shell_agent.py
@@ -1,5 +1,6 @@
-
-
-class ShellAgent(): 
-    pass
-
+from langchain_core.messages import AIMessage
+from langgraph.graph import END, START, StateGraph
+from langchain_ollama.chat_models import ChatOllama
+from states.graph_state import ShellState
+from agent_factory import agent_factory
+import asyncio
