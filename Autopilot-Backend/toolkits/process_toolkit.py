37f5fac6c088a01d29d9f3bf15007ca5fbc87ebb
Mhijazi16 <gultimate0@gmail.com>
2025-01-09 11:17:50 +0200

    create start_system_service tool

--- Autopilot-Backend/toolkits/process_toolkit.py
+++ Autopilot-Backend/toolkits/process_toolkit.py
@@ -23,3 +23,31 @@ def status_system_service(name: str):
     finally:
         send_to_terminal(output)
         return output
+
+def start_system_service(name: str): 
+    """
+        this tool is used to start an 
+        a service on the linux machine 
+        the command used is : 
+        `sudo systemctl start <service_name>`
+        takes in the name of the server
+        Args: 
+            name: str 
+        for example if you want to start 
+        ssh service you should 
+        provide the name sshd
+    """
+
+    try:
+        command = f"sudo systemctl start {name}"
+        output = ""
+        start_terminal(command)
+        process = pexpect.spawn(command)
+        process = handle_sudo(process)
+        output = f"✅ {name} Service Successfull Started .\n"
+        status_system_service(name)
+    except: 
+        output = f"🚨 faild Starting {name} service.\n"
+    finally: 
+        send_to_terminal(output)
+        return output
