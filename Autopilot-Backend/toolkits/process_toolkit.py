ce6c92f026cd089a6f4d4c3df070c0686fe0bb8b
Mhijazi16 <gultimate0@gmail.com>
2025-01-11 13:22:59 +0200

    created command_factory for getting services commands

--- Autopilot-Backend/toolkits/process_toolkit.py
+++ Autopilot-Backend/toolkits/process_toolkit.py
@@ -1,53 +1,29 @@
-from .shell_toolkit import handle_sudo, send_to_terminal, start_terminal
+from .shell_toolkit import handle_sudo, read_status, send_to_terminal, start_terminal
 import pexpect
 import os
 
-def status_system_service(name: str):
-    """
-    Get the status of a service on the Linux machine using:
-    `sudo systemctl status <service_name>`
-
-    Args:
-        name (str): Name of the service (e.g., 'sshd' for SSH service).
-
-    Returns:
-        str: Success or failure message.
-    """
-    output = ""
+def command_factory(name, action): 
+    command = ""
+    status = f"sudo systemctl status {name} | head -30"
     try:
-        command = f"systemctl status {name} | head"
-        start_terminal(command)
-        output = os.popen(command).read()
-    except Exception:
-        output = f"🚨 Failed to Retrieve Status of {name} Service.\n"
+        if "start" in action:
+            command = f"sudo systemctl start {name}"
+        elif "stop" in action:
+            command = f"sudo systemctl stop {name}"
+        elif "restart" in action:
+            command = f"sudo systemctl restart {name}"
+        elif "reload" in action:
+            command = f"sudo systemctl reload {name}"
+        elif "status" in action:
+            command = status
+        elif "enable" in action:
+            command = f"sudo systemctl enable {name}"
+        elif "disable" in action:
+            command = f"sudo systemctl disable {name}"
+        else:
+            raise ValueError(f"Unsupported action: {action}")
+    except Exception as e:
+        print(f"[ERROR] no suitable action! {e}")
     finally:
-        send_to_terminal(output)
-        return output
+        return command
 
-def start_system_service(name: str): 
-    """
-        this tool is used to start an 
-        a service on the linux machine 
-        the command used is : 
-        `sudo systemctl start <service_name>`
-        takes in the name of the server
-        Args: 
-            name: str 
-        for example if you want to start 
-        ssh service you should 
-        provide the name sshd
-    """
-
-    try:
-        command = f"sudo systemctl start {name}"
-        output = ""
-        start_terminal(command)
-        process = pexpect.spawn(command)
-        process = handle_sudo(process)
-        output = f"✅ {name} Service Successfull Started .\n"
-        status_system_service(name)
-    except: 
-        output = f"🚨 faild Starting {name} service.\n"
-    finally: 
-        send_to_terminal(output)
-        return output
