69edbaf4a6175f308769c3a74e97a019893da5ef
momen <momenmarwanh@gmail.com>
2025-01-14 17:29:25 +0200

    add chat websocket

--- Autopilot-Frontend/Frontend/autopilot/src/App.js
+++ Autopilot-Frontend/Frontend/autopilot/src/App.js
@@ -1,22 +1,44 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import ReconnectingWebSocket from "reconnecting-websocket"; 
 import Sidebar from "./components/Sidebar/Sidebar";
 import Chatbot from "./components/Chatbot/Chatbot";
 import Dashboard from "./components/Dashboard/Dashboard";
 import "./App.css";
-import blueLightTop from './assets/blue-light-top.svg';
+import blueLightTop from "./assets/blue-light-top.svg";
 import Settings from "./components/Settings/Settings";
-import Tasks from "./components/Tasks/Tasks"
+import Tasks from "./components/Tasks/Tasks";
 
 function App() {
   const [theme, setTheme] = useState("dark");
   const [taskNotificationStatus, setTaskNotificationStatus] = useState("");
   const [notifications, setNotifications] = useState([]);
 
+  const [chatWs, setChatWs] = useState(null);
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", theme === "dark");
   }, [theme]);
+
+  useEffect(() => {
+    const ws2 = new ReconnectingWebSocket("ws://127.0.0.1:8000/chat");
+  
+    setChatWs(ws2);
+  
+    ws2.onopen = () => {
+      console.log("Chat WebSocket connection established.");
+    };
+  
+    ws2.onerror = (err) => {
+      console.error("Chat WebSocket error:", err);
+    };
+  
+    return () => {
+      console.log("Cleaning up WebSocket connection.");
+      ws2.close();
+    };
+  }, [setChatWs]); 
+  
 
   return (
     <Router>
@@ -25,42 +47,59 @@ function App() {
         <div className="content">
           <div className="main-content">
             <Routes>
-              <Route path="/" element={<Chatbot />} />
+              <Route
+                path="/"
+                element={
+                  <Chatbot 
+                    chatWs={chatWs} 
+                    setChatWs={setChatWs} 
+                  />
+                }
+              />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route
                 path="/settings"
                 element={<Settings theme={theme} setTheme={setTheme} />}
               />
-              <Route path="/tasks" element={
-                <Tasks setNotifications={setNotifications}
-                  notifications={notifications}
-                  setTaskNotificationStatus={setTaskNotificationStatus}
-                />}
+              <Route
+                path="/tasks"
+                element={
+                  <Tasks
+                    setNotifications={setNotifications}
+                    notifications={notifications}
+                    setTaskNotificationStatus={setTaskNotificationStatus}
+                  />
+                }
               />
-
             </Routes>
           </div>
         </div>
-        <div className="notification-message-container">
-        {notifications.map((notification) => (
-          <div
-            key={notification.id}
-            className={`notification-message ${notification.status}`}
-          >
-            <div className="notification-message-icon">
-              {notification.status === "running" && <span className="spinner-notif"></span>}
-              {notification.status === "finished" && <span className="icon">✔</span>}
-              {notification.status === "failed" && <span className="icon">✖</span>}
-            </div>
-            <div>{notification.message}</div>
-          </div>
-        ))}
-      </div>
 
+        <div className="notification-message-container">
+          {notifications.map((notification) => (
+            <div
+              key={notification.id}
+              className={`notification-message ${notification.status}`}
+            >
+              <div className="notification-message-icon">
+                {notification.status === "running" && (
+                  <span className="spinner-notif"></span>
+                )}
+                {notification.status === "finished" && (
+                  <span className="icon">✔</span>
+                )}
+                {notification.status === "failed" && (
+                  <span className="icon">✖</span>
+                )}
+              </div>
+              <div>{notification.message}</div>
+            </div>
+          ))}
+        </div>
 
         <div className="fade-overlay"></div>
         <div className="blue-light-top">
-          <img src={blueLightTop} alt="blue-light" className="blue-light-pic"/>
+          <img src={blueLightTop} alt="blue-light" className="blue-light-pic" />
         </div>
         <div className="blue-light-bottom"></div>
       </div>
